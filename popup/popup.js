75172007683fd4d2a9efb10fe9d97d0f9976f735
quan.nguyenanh <quan.nguyenanh@pycogroup.com>
2019-09-27 19:49:54 +0700

    .

---
+++ popup/popup.js
@@ -0,0 +1,18 @@
+// checkLogworkTime() && alert("Relax and Logwork Please! ^^");
+const remindLogWork = setInterval(function () {
+  if (checkLogworkTime()) {
+    alert(alertify)
+    clearInterval(remindLogWork);
+  }
+}, 1000)
+
+function getTimeNow() {
+  const today = new Date();
+  const time = today.getHours() + ":" + today.getMinutes();
+  return time;
+}
+
+function checkLogworkTime() {
+  const currentTime = getTimeNow();
+  return (currentTime === "9:11");
+}
